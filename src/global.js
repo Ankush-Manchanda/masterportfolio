14ce22614b0f5d8e0c1e4f7be5eecaa67db703d6
Ankush-devhub <ankushmanchanda111@gmail.com>
2025-07-02 19:47:16 +0530

    .................

--- src/global.js
+++ src/global.js
@@ -9,14 +9,15 @@ export const GlobalStyles = createGlobalStyle`
 
   body {
     align-items: center;
+    justify-content: center;
+    flex-direction: column;
+    display: flex;
+    height: 100vh;
+    margin: 0;
+    padding: 0;
     background: ${({ theme }) => theme.body};
     color: ${({ theme }) => theme.text};
-    display: flex;
-    // flex-direction: column;
-    // justify-content: center;
-    // height: 100vh;
-    // margin: 0;
-    // padding: 0;
     font-family: BlinkMacSystemFont, -apple-system, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif;
     transition: all 0.25s linear;
-  }`;
+  }
+`;
